2803e396d5b9d9b5dc34f1c9b75fa5fa5724251b
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-09-26 23:54:55 +0200

    node environment variable

--- api/data-access/mangodbAccess.js
+++ api/data-access/mangodbAccess.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
 const persistentDataAccess = (collectionName) => {
-  const uri = MANGODB_URI;
+  const uri = process.env.MONGODB_URI;
 
   const client = new MongoClient(uri);
 
