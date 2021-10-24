91edae48cabe14df3de22e87dde1bb0d5c019aae
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-10-24 09:32:40 +0200

    rate limitter

--- api/index.js
+++ api/index.js
@@ -5,9 +5,20 @@ const registerRoute = require('./routes/register');
 const loginRoute = require('./routes/login');
 const authenticateUser = require('./middleware/authenticate');
 
+// const rateLimit = require('express-rate-limit');
+
 // const app = express();
 
 const router = express.Router();
+
+//Rate limit is used to prevent brute force attacks
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 100, // limit each IP to 100 requests per windowMs
+// });
+
+// router.use(limiter);
+// router.set('trust proxy', 1);
 
 router.use((req, res, next) => {
   console.log('api! index');
